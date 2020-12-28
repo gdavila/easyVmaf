bff18005c84e40297d799266cb0531fe2ada50cd
Gabriel Davila <gdavila.revelo@gmail.com>
2020-12-28 12:31:43 -0300

    Update config.py

--- config.py
+++ config.py
@@ -28,10 +28,9 @@ ffprobe = '/usr/local/bin/ffprobe'
 # uncomment for vmaf version < v2.0.0.0
 #vmaf_4K = '/usr/local/share/model/vmaf_4k_v0.6.1.pkl'
 #vmaf_HD = '/usr/local/share/model/vmaf_v0.6.1.pkl'
-#vmaf_HDneg = '/usr/local/share/model/vmaf_v0.6.1neg.pkl'
 
 
 # vmaf v2.0.0
 vmaf_4K = '/usr/local/share/model/vmaf_4k_v0.6.1.json'
 vmaf_HD = '/usr/local/share/model/vmaf_v0.6.1.json'
-vmaf_HDneg = '/usr/local/share/model//vmaf_v0.6.1neg.json'
+vmaf_HDneg = '/usr/local/share/model/vmaf_v0.6.1neg.json'
