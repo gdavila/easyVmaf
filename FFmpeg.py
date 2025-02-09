e970ae9db7dc40897a09e4fbdd53df451a82eb82
gdavila <gabriel.davila@bitmovin.com>
2025-02-09 10:07:36 -0300

    Copyright updated

--- FFmpeg.py
+++ FFmpeg.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2020 Gabriel Davila - gdavila.revelo@gmail.com
+Copyright (c) 2020 Gabriel Davila - https://github.com/gdavila
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
